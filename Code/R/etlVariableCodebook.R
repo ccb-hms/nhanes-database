ee08b6a7bdd6dd4c642f7e204f70ddfb9fff8482
Nathan Palmer <nathan_palmer@hms.harvard.edu>
2023-01-12 14:54:22 -0500

    added code to load variable codebooks and table / variable descriptions. x

--- Code/R/etlVariableCodebook.R
+++ Code/R/etlVariableCodebook.R
@@ -5,69 +5,56 @@
 #
 # run this from the root of the NHANES-metadata repository
 
-# enumerate files to be processed
-fileList = list.files("metadata/variable_codebooks", full.names=TRUE)
-
 # this is the location of the combined output file
-outFile = paste(sep = "/", getwd(), "VariableCodebook.txt")
-
-# remove any existing output
-if (file.exists(outFile)) {
-    file.remove(outFile)
-}
-
-file.create(outFile)
-
-# for each of the input files
-for (x in fileList) {
-    
-    # read the table
-    tab = read.table(x, header=TRUE, sep=",")
-    
-    # the SEQN files are broken
-    if (ncol(tab) == 1) {
-        next
-    }
-    
-    if (ncol(tab) != 5) {
-        stop(paste0("The file \n", x, "\ndid not contain 5 columns"))
-    }
-    
-    # split the full file name on directory separator
-    splits = strsplit(x, split="/")[[1]]
-    
-    # parse out the table and variable names from the file name
-    fName = splits[length(splits)]
-    tableName = strsplit(fName, "_[A-Za-z0-9]+\\.csv", fixed=FALSE)[[1]]
-    variableName = gsub(gsub(x = fName, pattern = paste0(tableName, "_"), replacement = ""), pattern = ".csv", replacement = "")
-    
-    # append columns containing the table and variable names
-    tab = cbind(rep(variableName, times = nrow(tab)), tab)
-    tab = cbind(rep(tableName, times = nrow(tab)), tab)
-    
-    # append to the output file
-    write.table(x = tab, append = TRUE, sep = "\t", row.names = FALSE, col.names = FALSE, file = outFile, quote=FALSE)
-}
+codebookFile = paste(sep = "/", getwd(), "metadata/nhanes_variables_codebooks.csv")
+tablesFile = paste(sep = "/", getwd(), "metadata/nhanes_tables.csv")
+variablesFile = paste(sep = "/", getwd(), "metadata/nhanes_variables.csv")
 
 # parameters to connect to SQL
 sqlHost = "localhost"
 sqlUserName = "sa"
 sqlPassword = "yourStrong(!)Password"
-sqlDefaultDb = "master"
+sqlDefaultDb = "NhanesLandingZone"
 
-# connect to SQL
-cn = MsSqlTools::connectMsSqlSqlLogin(
-    server = sqlHost, 
-    user = sqlUserName, 
-    password = sqlPassword, 
-    database = sqlDefaultDb
-)
+# loop waiting for SQL Server database to become available
+for (i in 1:20) {
+    cn = tryCatch(
+        # connect to SQL
+        MsSqlTools::connectMsSqlSqlLogin(
+            server = sqlHost, 
+            user = sqlUserName, 
+            password = sqlPassword, 
+            database = sqlDefaultDb
+        ), warning = function(e) {
+            return(NA)
+        }, error = function(e) {
+            return(NA)
+        }
+    )
+    
+    suppressWarnings({
+         if (is.na(cn)) {
+            Sys.sleep(6)
+        } else {
+            break
+        }
+    })
+   
+}
 
-# create the table in SQL
+suppressWarnings({
+    if (is.na(cn)) {
+        stop("could not connect to SQL Server")
+    }
+})
+
+# load the codebook
+
+# create the VariableCodebook table in SQL
 SqlTools::dbSendUpdate(cn, "
     CREATE TABLE NhanesLandingZone.dbo.VariableCodebook (
-        Questionnaire varchar(64),
         Variable varchar(64),
+        Questionnaire varchar(64),
         CodeOrValue varchar(64),
         ValueDescription varchar(256),
         Count int,
@@ -77,10 +64,122 @@ SqlTools::dbSendUpdate(cn, "
 ")
 
 # run bulk insert
-insertStatement = paste(sep="",
-    "BULK INSERT NhanesLandingZone.dbo.VariableCodebook FROM '",
-    outFile,
-    "' WITH (KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=1, FIELDTERMINATOR='\t')"
-)
+insertStatement = paste(sep="", "
+    BULK INSERT NhanesLandingZone.dbo.VariableCodebook FROM '", codebookFile, "'
+    WITH (FORMAT='CSV', KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=2, FIELDTERMINATOR=',')
+")
 
 SqlTools::dbSendUpdate(cn, insertStatement)
+
+# shrink transaction log
+SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
+
+# issue checkpoint
+SqlTools::dbSendUpdate(cn, "CHECKPOINT")
+
+# load the table descriptions
+
+# create the nhanes_tables table in SQL
+SqlTools::dbSendUpdate(cn, "
+    CREATE TABLE ##tmp_nhanes_tables (
+        [Table] varchar(64),
+        TableName varchar(1024),
+        DataGroup varchar(64),
+        Year int
+    )
+")
+
+# run bulk insert
+insertStatement = paste(sep="", "
+    BULK INSERT ##tmp_nhanes_tables FROM '", tablesFile, "'
+    WITH (FORMAT='CSV', KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=2, FIELDTERMINATOR=',')
+")
+
+SqlTools::dbSendUpdate(cn, insertStatement)
+
+# clean up and insert in new table with consistent nomenclature
+SqlTools::dbSendUpdate(cn, "
+    SELECT 
+        Q.Questionnaire, 
+        T.TableName AS Description,
+        T.DataGroup,
+        Q.BeginYear,
+        Q.EndYear,
+        Q.TableName
+    INTO NhanesLandingZone.dbo.QuestionnaireDescriptions
+    FROM 
+        ##tmp_nhanes_tables T 
+        INNER JOIN NhanesLandingZone.dbo.QuestionnaireVariables Q ON
+            T.[Table] = Q.Questionnaire
+    GROUP BY
+        Q.Questionnaire, 
+        T.TableName,
+        T.DataGroup,
+        Q.BeginYear,
+        Q.EndYear,
+        Q.TableName
+")
+
+SqlTools::dbSendUpdate(cn, "DROP TABLE ##tmp_nhanes_tables")
+
+# shrink transaction log
+SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
+
+# issue checkpoint
+SqlTools::dbSendUpdate(cn, "CHECKPOINT")
+
+
+# load the variable descriptions
+
+# create the nhanes_tables table in SQL
+SqlTools::dbSendUpdate(cn, "
+    CREATE TABLE ##tmp_nhanes_variables (
+        Variable varchar(64),
+        [Table] varchar(64),
+        SasLabel varchar(64),
+        EnglishText varchar(1024),
+        Target varchar(128)
+    )
+")
+
+# run bulk insert
+insertStatement = paste(sep="", "
+    BULK INSERT ##tmp_nhanes_variables FROM '", variablesFile, "'
+    WITH (FORMAT='CSV', KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=2, FIELDTERMINATOR=',')
+")
+
+SqlTools::dbSendUpdate(cn, insertStatement)
+
+# add columns to QuestionnaireVariables table to accommodate additional data
+SqlTools::dbSendUpdate(cn, "
+    ALTER TABLE NhanesLandingZone.dbo.QuestionnaireVariables 
+    ADD 
+        Description varchar(1024) NULL, 
+        Target varchar(128) NULL,
+        SasLabel varchar(64)
+")
+
+# update the new columns in the NhanesLandingZone.dbo.QuestionnaireVariables
+# with values from the imported table
+SqlTools::dbSendUpdate(cn, "
+    UPDATE Q
+    SET 
+        Q.Description = V.EnglishText,
+        Q.Target = V.Target,
+        Q.SasLabel = V.SasLabel
+    FROM 
+        NhanesLandingZone.dbo.QuestionnaireVariables Q
+        INNER JOIN ##tmp_nhanes_variables V ON
+            Q.Questionnaire = V.[Table]
+            AND Q.Variable = V.Variable
+
+    SELECT * FROM NhanesLandingZone.dbo.QuestionnaireVariables
+")
+
+SqlTools::dbSendUpdate(cn, "DROP TABLE ##tmp_nhanes_variables")
+
+# shrink transaction log
+SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
+
+# issue checkpoint
+SqlTools::dbSendUpdate(cn, "CHECKPOINT")
