2d741fa34e092169ee0dfaa1f61a374cfcc66994
Sam Pullman <samantha_pullman@hms.harvard.edu>
2023-04-14 13:06:16 -0400

    added ENV variables, fixed SEQN questionnaire variable

--- Code/R/download.R
+++ Code/R/download.R
@@ -31,6 +31,9 @@ opt = optparse::parse_args(optParser);
 DOCKER_VERSION <- Sys.getenv("DOCKER_VERSION", TRUE)
 COLLECTION_DATE <- Sys.getenv("COLLECTION_DATE", TRUE)
 
+print(DOCKER_VERSION)
+print(COLLECTION_DATE)
+
 # this varaible is used below to determine how to handle errors.
 # if running in a container build process, any errors encountered
 # in the processing of the files should cause R to return non-zero
