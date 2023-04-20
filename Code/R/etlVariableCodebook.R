a2e8b601d870c919b485eb0569010cfadded35f5
Sam Pullman <samantha_pullman@hms.harvard.edu>
2023-04-20 13:05:17 -0400

    adding views

--- Code/R/etlVariableCodebook.R
+++ Code/R/etlVariableCodebook.R
@@ -285,3 +285,42 @@ for (currTable in ontology_mappings) {
 # issue checkpoint
 SqlTools::dbSendUpdate(cn, "CHECKPOINT")
 
+m = DBI::dbGetQuery(cn, "
+SELECT TABLE_NAME
+FROM INFORMATION_SCHEMA.TABLES
+WHERE 
+    TABLE_TYPE = 'BASE TABLE' 
+    AND TABLE_CATALOG='NhanesLandingZone'
+    AND TABLE_NAME != 'QuestionnaireVariables'
+    AND TABLE_NAME != 'DownloadErrors'
+    AND TABLE_NAME != 'VariableCodebook'
+    AND TABLE_NAME != 'QuestionnaireDescriptions'
+    AND TABLE_NAME != 'ontology_entailed_edges'
+    AND TABLE_NAME != 'ontology_labels'
+    AND TABLE_NAME != 'ontology_edges'
+    AND TABLE_NAME != 'nhanes_variables_mappings'
+")
+
+for (i in 1:nrow(m)) {
+    
+    currTableName = m[i,"TABLE_NAME"]
+    questionnaireLables = DBI::dbGetQuery(
+        cn, 
+        paste(
+            sep="", 
+            "SELECT Questionnaire FROM ", currTableName, " GROUP BY Questionnaire")
+        )
+    
+    for (j in 1:nrow(questionnaireLables)) {
+        
+        currQuestionnaire = questionnaireLables[j, "Questionnaire"]
+        DBI::dbGetQuery(
+            cn, 
+            paste(sep="", "CREATE VIEW ", currQuestionnaire, " AS SELECT * FROM ", currTableName, " WHERE Questionnaire = '", currQuestionnaire, "'"))
+    }
+}
+
+# shrink transaction log
+SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
+# issue checkpoint
+SqlTools::dbSendUpdate(cn, "CHECKPOINT")
