594a415522efa7a823f345cd7d1ed09b05328e18
Nathan Palmer <nathan.palmer@gmail.com>
2023-08-24 21:13:16 -0400

    include wait loop for SQL Server to start up

--- Code/R/download.R
+++ Code/R/download.R
@@ -44,13 +44,37 @@ sqlUserName = "sa"
 sqlPassword = "yourStrong(!)Password"
 sqlDefaultDb = "master"
 
-# connect to SQL
-cn = MsSqlTools::connectMsSqlSqlLogin(
-  server = sqlHost, 
-  user = sqlUserName, 
-  password = sqlPassword, 
-  database = sqlDefaultDb
-)
+# loop waiting for SQL Server database to become available
+for (i in 1:60) {
+    cn = tryCatch(
+        # connect to SQL
+        MsSqlTools::connectMsSqlSqlLogin(
+            server = sqlHost, 
+            user = sqlUserName, 
+            password = sqlPassword, 
+            database = sqlDefaultDb
+        ), warning = function(e) {
+            return(NA)
+        }, error = function(e) {
+            return(NA)
+        }
+    )
+    
+    suppressWarnings({
+         if (is.na(cn)) {
+            Sys.sleep(10)
+        } else {
+            break
+        }
+    })
+   
+}
+
+suppressWarnings({
+    if (is.na(cn)) {
+        stop("could not connect to SQL Server")
+    }
+})
 
 # control persistence of downloaded and extracted text files
 persistTextFiles = FALSE
