7775fdedb5fff0d62a9c8ff3f366b6ddd4dfe6f2
Nathan Palmer <nathan.palmer@gmail.com>
2023-10-31 15:26:50 -0400

    fixed setting of SA_PASSWORD

--- Container/startup.sh
+++ Container/startup.sh
@@ -28,7 +28,8 @@ then
 	runuser -m -p  mssql -c '/opt/mssql/bin/sqlservr &'
 	
 	# make the SA password available to all users
-	RUN echo "SA_PASSWORD=$SA_PASSWORD" >> /etc/environment
+	echo "SA_PASSWORD=$SA_PASSWORD" >> /etc/environment
+	echo "SA_PASSWORD=$SA_PASSWORD" >> /usr/local/lib/R/etc/Renviron
 fi
 
 # start RStudio Server
