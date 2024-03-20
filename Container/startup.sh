49240497b9312d59883e4ec4cb068f5a95468570
Nathan Palmer <nathan_palmer@hms.harvard.edu>
2024-03-19 23:40:46 -0400

    Dockerfile.MariaDB extends official MariaDB container, adds sshd and modifies the contianer startup process to create a new user, run sshd, start the database services, provision the columnstore cluster to include only localhost, and then block.

--- Container/startup.sh
+++ Container/startup.sh
@@ -6,35 +6,19 @@ if [[ -z $CONTAINER_USER_USERNAME ]] || [[ -z $CONTAINER_USER_PASSWORD ]];
 then
       exit 1
 else
-    groupadd rstudio-users
-
     useradd $CONTAINER_USER_USERNAME \
-	&& mkdir /home/${CONTAINER_USER_USERNAME} \
-	&& chown ${CONTAINER_USER_USERNAME}:${CONTAINER_USER_USERNAME} /home/${CONTAINER_USER_USERNAME} \
-	&& chown ${CONTAINER_USER_USERNAME}:${CONTAINER_USER_USERNAME} /HostData \
-	&& addgroup ${CONTAINER_USER_USERNAME} staff \
 	&& echo "$CONTAINER_USER_USERNAME:$CONTAINER_USER_PASSWORD" | chpasswd \
-	&& adduser ${CONTAINER_USER_USERNAME} sudo \
+	&& usermod -aG wheel $CONTAINER_USER_USERNAME \
 	&& chsh -s /bin/bash ${CONTAINER_USER_USERNAME}
-
-    usermod -a -G rstudio-users $CONTAINER_USER_USERNAME
-fi
-
-# ACCEPT_EULA needs to be set to 'Y'
-# and SA_PASSWORD needs to be not null (actually needs to pass complexity requirements, but not testing here)
-# to run SQL Server
-if [[ $ACCEPT_EULA == "Y" && ! -z $SA_PASSWORD ]]
-then
-	export MSSQL_SA_PASSWORD=$SA_PASSWORD
-	runuser -m -p  mssql -c '/opt/mssql/bin/sqlservr  --accept-eula --reset-sa-password &'
-		
-	# make the SA password available to all users
-	echo "SA_PASSWORD=\"$SA_PASSWORD\"" >> /etc/environment
-	echo "SA_PASSWORD=\"$SA_PASSWORD\"" >> /usr/local/lib/R/etc/Renviron.site
 fi
 
 # start sshd
-/usr/sbin/sshd -D &
+/usr/sbin/sshd -D&
 
-# rocker/shiny runs this by default
-/init
+# start the MariaDB services in the background
+start-services&
+
+# provision the MariaDB Columnstore service when it becomes available
+provision
+
+wait
