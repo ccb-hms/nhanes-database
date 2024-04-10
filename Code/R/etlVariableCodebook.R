b0662ee871e782abcd2acd0b4efa8a26c5238947
Nathan Palmer <nathan_palmer@hms.harvard.edu>
2024-04-10 07:35:06 -0400

    only ontology tables remaining for import refactor

--- Code/R/etlVariableCodebook.R
+++ Code/R/etlVariableCodebook.R
@@ -17,26 +17,25 @@ ontologyTables = paste(sep = "/", getwd(), "ontology-tables/")
 
 # parameters to connect to SQL
 sqlHost = "localhost"
-sqlUserName = "sa"
-sqlPassword = "yourStrong(!)Password"
-sqlDefaultDb = "NhanesLandingZone"
+sqlUserName = "admin"
+sqlPassword = "C0lumnStore!"
 
-# loop waiting for SQL Server database to become available
-for (i in 1:60) {
+# loop waiting for SQL database to become available
+for (i in 1:12) {
     cn = tryCatch(
         # connect to SQL
-        MsSqlTools::connectMsSqlSqlLogin(
-            server = sqlHost, 
-            user = sqlUserName, 
-            password = sqlPassword, 
-            database = sqlDefaultDb
-        ), warning = function(e) {
+        RMariaDB::dbConnect(
+          drv=RMariaDB::MariaDB(),
+          username=sqlUserName,
+          password=sqlPassword,
+          host=sqlHost
+        )
+        , warning = function(e) {
             return(NA)
         }, error = function(e) {
             return(NA)
         }
     )
-    
     suppressWarnings({
          if (is.na(cn)) {
             Sys.sleep(10)
@@ -44,20 +43,21 @@ for (i in 1:60) {
             break
         }
     })
-   
 }
 
 suppressWarnings({
     if (is.na(cn)) {
-        stop("could not connect to SQL Server")
+        stop("could not connect to SQL database")
     }
 })
 
+#-----------------------------------------------------------------
 # load the codebook
+#-----------------------------------------------------------------
 
 # create the VariableCodebook table in SQL
-SqlTools::dbSendUpdate(cn, "
-    CREATE TABLE NhanesLandingZone.Metadata.VariableCodebook (
+DBI::dbExecute(cn, "
+    CREATE TABLE NhanesMetadata.VariableCodebook (
         Variable varchar(64),
         TableName varchar(64),
         CodeOrValue varchar(64),
@@ -69,24 +69,18 @@ SqlTools::dbSendUpdate(cn, "
 ")
 
 insertStatement = paste(sep="", "
-    BULK INSERT NhanesLandingZone.Metadata.VariableCodebook FROM '", codebookFile, "'
-    WITH (FORMAT='CSV', KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=2, FIELDTERMINATOR='\t')
-")
+    LOAD DATA INFILE '", codebookFile, "' INTO TABLE NhanesMetadata.VariableCodebook FIELDS OPTIONALLY ENCLOSED BY '\"' IGNORE 1 ROWS;"
+)
+DBI::dbExecute(cn, insertStatement)
 
-SqlTools::dbSendUpdate(cn, insertStatement)
-
-# shrink transaction log
-SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
-
-# issue checkpoint
-SqlTools::dbSendUpdate(cn, "CHECKPOINT")
-
+#-----------------------------------------------------------------
 # load the table descriptions
+#-----------------------------------------------------------------
 
 # create the nhanes_tables table in SQL
-SqlTools::dbSendUpdate(cn, "
-    CREATE TABLE ##tmp_nhanes_tables (
-        [Table] varchar(64),
+DBI::dbExecute(cn, "
+    CREATE TEMPORARY TABLE NhanesMetadata.tmp_nhanes_tables (
+        `Table` varchar(64),
         TableName varchar(1024),
         BeginYear int,
         EndYear int,
@@ -100,69 +94,54 @@ SqlTools::dbSendUpdate(cn, "
 
 # run bulk insert
 insertStatement = paste(sep="", "
-    BULK INSERT ##tmp_nhanes_tables FROM '", tablesFile, "'
-    WITH (FORMAT='CSV', KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=2, FIELDTERMINATOR='\t')
-")
-
-SqlTools::dbSendUpdate(cn, insertStatement)
-
-# As of v0.0.2, the nhanes_tables.tsv file includes doublequotes in the values. These lines replace them. 
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_tables SET [Table] = REPLACE([Table], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_tables SET [TableName] = REPLACE([TableName], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_tables SET [DataGroup] = REPLACE([DataGroup], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_tables SET [UseConstraints] = REPLACE([UseConstraints], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_tables SET [DocFile] = REPLACE([DocFile], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_tables SET [DataFile] = REPLACE([DataFile], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_tables SET [DatePublished] = REPLACE([DatePublished], CHAR(34), '')")
+    LOAD DATA INFILE '", tablesFile, "' INTO TABLE NhanesMetadata.tmp_nhanes_tables FIELDS OPTIONALLY ENCLOSED BY '\"' IGNORE 1 ROWS;"
+)
+DBI::dbExecute(cn, insertStatement)
 
 # clean up and insert in new table with consistent nomenclature
-SqlTools::dbSendUpdate(cn, "
-    SELECT 
-        T.TableName AS Description,
-        Q.TableName,
-        T.BeginYear,
-        T.EndYear,
-        T.DataGroup,
-        T.UseConstraints,
-        T.DocFile,
-        T.DataFile,
-        T.DatePublished
-    INTO NhanesLandingZone.Metadata.QuestionnaireDescriptions
-    FROM 
-        ##tmp_nhanes_tables T 
-        INNER JOIN NhanesLandingZone.Metadata.QuestionnaireVariables Q ON
-            T.[Table] = Q.TableName
-    GROUP BY
-        T.TableName,
-        Q.TableName,
-        T.BeginYear,
-        T.EndYear,
-        T.DataGroup,
-        T.UseConstraints,
-        T.DocFile,
-        T.DataFile,
-        T.DatePublished
+DBI::dbExecute(cn, "
+    CREATE OR REPLACE TABLE NhanesMetadata.QuestionnaireDescriptions
+        SELECT 
+            T.TableName AS Description,
+            Q.TableName,
+            T.BeginYear,
+            T.EndYear,
+            T.DataGroup,
+            T.UseConstraints,
+            T.DocFile,
+            T.DataFile,
+            T.DatePublished
+        FROM 
+            NhanesMetadata.tmp_nhanes_tables T 
+            INNER JOIN NhanesMetadata.QuestionnaireVariables Q ON
+                T.`Table` = Q.TableName
+        GROUP BY
+            T.TableName,
+            Q.TableName,
+            T.BeginYear,
+            T.EndYear,
+            T.DataGroup,
+            T.UseConstraints,
+            T.DocFile,
+            T.DataFile,
+            T.DatePublished
 ")
 
-SqlTools::dbSendUpdate(cn, "DROP TABLE ##tmp_nhanes_tables")
+DBI::dbExecute(cn, "DROP TABLE NhanesMetadata.tmp_nhanes_tables")
 
-# shrink transaction log
-SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
-
-# issue checkpoint
-SqlTools::dbSendUpdate(cn, "CHECKPOINT")
-
+#-----------------------------------------------------------------
 # load the variable descriptions
+#-----------------------------------------------------------------
 
 # create the nhanes_tables table in SQL
-SqlTools::dbSendUpdate(cn, "
-    CREATE TABLE ##tmp_nhanes_variables (
+DBI::dbExecute(cn, "
+    CREATE TEMPORARY TABLE NhanesMetadata.tmp_nhanes_variables (
         Variable varchar(64),
-        [Table] varchar(64),
+        `Table` varchar(64),
         SASLabel varchar(64),
         EnglishText varchar(1024),
-        UseConstraints varchar(max),
-        Target varchar(max),
+        UseConstraints varchar(4096),
+        Target varchar(4096),
         Tags varchar(1024),
         ProcessedText varchar(1024),
         VariableID varchar(1024),
@@ -171,75 +150,45 @@ SqlTools::dbSendUpdate(cn, "
     )
 ")
 
-
 # run bulk insert
 insertStatement = paste(sep="", "
-    BULK INSERT ##tmp_nhanes_variables FROM '", variablesFile, "'
-    WITH (FORMAT='CSV', KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=2, FIELDTERMINATOR='\t')
-")
+    LOAD DATA INFILE '", variablesFile, "' INTO TABLE NhanesMetadata.tmp_nhanes_variables FIELDS OPTIONALLY ENCLOSED BY '\"' IGNORE 1 ROWS;"
+)
+DBI::dbExecute(cn, insertStatement)
 
-SqlTools::dbSendUpdate(cn, insertStatement)
-
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [Variable] = REPLACE([Variable], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [Table] = REPLACE([Table], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [SasLabel] = REPLACE([SasLabel], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [EnglishText] = REPLACE([EnglishText], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [Target] = REPLACE([Target], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [UseConstraints] = REPLACE([UseConstraints], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [ProcessedText] = REPLACE([ProcessedText], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [Tags] = REPLACE([Tags], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [VariableID] = REPLACE([VariableID], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [IsPhenotype] = REPLACE([IsPhenotype], CHAR(34), '')")
-SqlTools::dbSendUpdate(cn, "UPDATE ##tmp_nhanes_variables SET [OntologyMapped] = REPLACE([OntologyMapped], CHAR(34), '')")
-
-# add columns to QuestionnaireVariables table to accommodate additional data
-SqlTools::dbSendUpdate(cn, "
-    ALTER TABLE NhanesLandingZone.Metadata.QuestionnaireVariables 
-    ADD 
-        Description varchar(1024) NULL, 
-        Target varchar(max) NULL,
-        SasLabel varchar(64),
-        UseConstraints varchar(max),
-        ProcessedText varchar(1024),
-        Tags varchar(1024),
-        VariableID varchar(1024),
-        IsPhenotype varchar(1024),
-        OntologyMapped varchar(1024)
-")
-
-# update the new columns in the NhanesLandingZone.dbo.QuestionnaireVariables
-# with values from the imported table
+# rename the QuestionnaireVariables -- we are going to recreate it by joining to tmp_nhanes_variables
+# to include additional columns 
+DBI::dbExecute(cn, "RENAME TABLE NhanesMetadata.QuestionnaireVariables TO NhanesMetadata.QuestionnaireVariables_old")
 
 #TODO: in v0.4.0, for some reason lines 222-223 don't import correctly. When switched they load correctly. 
 #May need to update this in future releases.
-SqlTools::dbSendUpdate(cn, "
-    UPDATE Q
-    SET 
-        Q.Description = V.EnglishText,
-        Q.Target = V.Target,
-        Q.SasLabel = V.SasLabel,
-        Q.UseConstraints = V.UseConstraints,
-        Q.ProcessedText = V.ProcessedText,
-        Q.Tags = V.VariableID,
-        Q.VariableID = V.Tags,
-        Q.IsPhenotype = V.IsPhenotype,
-        Q.OntologyMapped = V.OntologyMapped   
-    FROM 
-        NhanesLandingZone.Metadata.QuestionnaireVariables Q
-        INNER JOIN ##tmp_nhanes_variables V ON
-            Q.TableName = V.[Table]
+DBI::dbExecute(cn, "
+    CREATE OR REPLACE TABLE NhanesMetadata.QuestionnaireVariables
+    SELECT 
+        Q.TableName,
+        Q.Variable,
+        V.EnglishText AS Description,
+        V.Target,
+        V.SasLabel,
+        V.UseConstraints,
+        V.ProcessedText,
+        V.VariableID,
+        V.Tags,
+        V.IsPhenotype,
+        V.OntologyMapped   
+    FROM NhanesMetadata.QuestionnaireVariables_old Q
+        INNER JOIN NhanesMetadata.tmp_nhanes_variables V ON
+            Q.TableName = V.`Table`
             AND Q.Variable = V.Variable
 ")
 
-SqlTools::dbSendUpdate(cn, "DROP TABLE ##tmp_nhanes_variables")
+# manually update these
+DBI::dbExecute(cn, "UPDATE NhanesMetadata.QuestionnaireVariables SET Description =  'Respondent sequence number', SasLabel =  'Respondent sequence number' WHERE Variable = 'SEQN'")
 
-# shrink transaction log
-SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
+DBI::dbExecute(cn, "DROP TABLE NhanesMetadata.tmp_nhanes_variables")
+DBI::dbExecute(cn, "DROP TABLE NhanesMetadata.QuestionnaireVariables_old")
 
-SqlTools::dbSendUpdate(cn, "UPDATE [NhanesLandingZone].[Metadata].[QuestionnaireVariables] SET Description =  'Respondent sequence number', SasLabel =  'Respondent sequence number' WHERE Variable = 'SEQN'")
-
-# issue checkpoint
-SqlTools::dbSendUpdate(cn, "CHECKPOINT")
+# TODO: debugged to here
 
 #--------------------------------------------------------------------------------------------------------
 # Add Ontology Tables
@@ -267,7 +216,7 @@ for (currTable in ontology_tables) {
 
     print(createTableQuery)
     # create the table in SQL
-    SqlTools::dbSendUpdate(cn, createTableQuery)
+    DBI::dbExecute(cn, createTableQuery)
     print("no problem creating")
     
     # run bulk insert
@@ -278,7 +227,7 @@ for (currTable in ontology_tables) {
                             paste0(path, currTable),
                             "' WITH (KEEPNULLS, TABLOCK, ROWS_PER_BATCH=2000, FIRSTROW=2, FIELDTERMINATOR = '\t', ROWTERMINATOR = '\n')"
     )
-    SqlTools::dbSendUpdate(cn, insertStatement)
+    DBI::dbExecute(cn, insertStatement)
     print("no problem inserting")
     
   # keep memory as clean as possible
@@ -288,9 +237,9 @@ for (currTable in ontology_tables) {
 }
 
 # shrink transaction log
-SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
+DBI::dbExecute(cn, "DBCC SHRINKFILE(Nhaneslog)")
 # issue checkpoint
-SqlTools::dbSendUpdate(cn, "CHECKPOINT")
+DBI::dbExecute(cn, "CHECKPOINT")
 
 # --------------------------------------------------------------------------------------------------------
 # Add Ontology Mappings
@@ -321,7 +270,7 @@ for (currTable in ontology_mappings) {
         createTableQuery = gsub(createTableQuery, pattern = '"', replace = "", fixed = TRUE)
         
         # create the table in SQL
-        SqlTools::dbSendUpdate(cn, createTableQuery)
+        DBI::dbExecute(cn, createTableQuery)
 
         # run bulk insert
         if (currTable == "nhanes_oral_health_mappings.tsv") {
@@ -343,7 +292,7 @@ for (currTable in ontology_mappings) {
         ) 
         }
         
-        SqlTools::dbSendUpdate(cn, insertStatement)
+        DBI::dbExecute(cn, insertStatement)
 
     # keep memory as clean as possible
     rm(loaded_data)
@@ -352,22 +301,7 @@ for (currTable in ontology_mappings) {
 }
 
 # shrink transaction log
-SqlTools::dbSendUpdate(cn, "DBCC SHRINKFILE(NhanesLandingZone_log)")
-
-# issue checkpoint
-SqlTools::dbSendUpdate(cn, "CHECKPOINT")
-
-# shrink tempdb
-SqlTools::dbSendUpdate(cn, "USE tempdb")
-
-tempFiles = DBI::dbGetQuery(cn, "
-                        SELECT name FROM TempDB.sys.sysfiles
-                        ")
-
-for (i in 1:nrow(tempFiles)) {    
-    currTempFileName = tempFiles[i,1]
-    SqlTools::dbSendUpdate(cn, paste("DBCC SHRINKFILE(",currTempFileName,", 8)", sep=''))
-}
+DBI::dbExecute(cn, "FLUSH BINARY LOGS")
 
 # shutdown the database engine cleanly
-SqlTools::dbSendUpdate(cn, "SHUTDOWN")
+DBI::dbExecute(cn, "SHUTDOWN")
